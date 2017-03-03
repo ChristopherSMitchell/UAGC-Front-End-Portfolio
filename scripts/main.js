6f7a5c71a05e25408c1824bc52059a14c7a05871
MrMPerial <chris@csmitchell.com>
2017-03-03 11:09:11 -0600

    Fixed page issues

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,33 @@
-$(document).ready(function() {
-  $('#home').show();
-  $('#about').hide();
-  $('#contact').hide();
-  $('#portfolioPage').hide();
-  
-  $('#homeButton').on('click', function() {
-    $('#home').fadeIn(1000);
+$(document).ready(function () {
+    $('#home').show();
     $('#about').hide();
     $('#contact').hide();
-  });
-  $('#aboutButton').on('click', function() {
-    $('#about').fadeIn(1000);
-    $('#home').hide();
-    $('#contact').hide();
-  });
-  
-  $('#contactButton').on('click', function() {
-    $('#contact').fadeIn(1000);
-    $('#about').hide();
-    $('#home').hide();
-  });
-  
-  $('#portfolioButton').on('click', function() {
-    $('#portfolioPage').slideToggle(1000);
-  });
+    $('#portfolioPage').hide();
+    
+    $('#homeButton').on('click', function () {
+        $('#home').fadeIn(1000);
+        $('#about').hide();
+        $('#contact').hide();
+        $('#portfolioPage').hide();
+    });
+    $('#aboutButton').on('click', function () {
+        $('#about').fadeIn(1000);
+        $('#home').hide();
+        $('#contact').hide();
+        $('#portfolioPage').hide();
+    });
+
+    $('#contactButton').on('click', function () {
+        $('#contact').fadeIn(1000);
+        $('#about').hide();
+        $('#home').hide();
+        $('#portfolioPage').hide();
+    });
+
+    $('#portfolioButton').on('click', function () {
+        $('#portfolioPage').fadeIn(1000);
+        $('#contact').hide();
+        $('#about').hide();
+        $('#home').hide();
+    });
 });
